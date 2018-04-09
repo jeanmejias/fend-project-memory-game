dab5f47eb0acb097d93f9a9f31bf8e8b382d598b
JeanMejias <jean.eg.mejias@hotmail.com>
2018-04-09 17:19:12 +0100

    Code rethink. Start from the scratch

--- js/app.js
+++ js/app.js
@@ -1,18 +1,49 @@
-/*
- * Create a list that holds all of your cards
- */
+// cards array holds all cards
+let card = document.getElementsByClassName('card');
+let cards = [...card]
+console.log(cards);
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+
+const time = document.querySelector(".time");
+let timer;
+let minutes = document.querySelector(".minutes");
+let seconds = document.querySelector(".seconds");
+const pause = document.querySelector(".pause");
+const play = document.querySelector(".play");
+const restart = document.querySelector(".restart");
+const tableElements = [diamond, diamond, paper-plane-o, paper-plane-o, anchor, anchor, bolt, bolt, cube, cube, leaf, leaf, bicycle, bicycle, bomb, bomb];
+
+
+//the play button
+play.onclick = function startTimer() {
+	timer = setInterval(function() {
+		seconds.textContent++;
+		if(seconds.textContent == 60) {
+      seconds.textContent = 0;
+      minutes.textContent++;
+		}
+	}, 1000);
+return timer;
+}
+
+//the pause button
+pause.onclick = function stopTimer () {
+	clearInterval(timer);
+}
+
+//restart the game
+restart.onclick = function restartGame() {
+  minutes.textContent = 0;
+  seconds.textContent = 0;
+}
+
+//shuffle array
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length;
+    var temporaryValue;
+    var randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,14 +56,17 @@ function shuffle(array) {
     return array;
 }
 
+//flip cards
+card.addEventListener("click", flipCards() {
+       if(card.style.display === "none") {
+  card.style.display = "block";
+} else {
+  card.style.display = "none";
+}
+ });
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+//remove stars one by one
+function removeStars() {
+  document.querySelector(".stars li:first-child()").classList.remove("star-fade");
+	document.querySelector(".stars li:second-child()").classList.remove("star-fade");
+}
